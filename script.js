bec494be18ea36c051bacb9eb6ff9542121d7581
Kcirama <150374319+Kcirama@users.noreply.github.com>
2023-11-09 18:03:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+btn1.onclick = () => document.body.style.backgroundColor = "blue";
+btn2.onclick = () => document.body.style.backgroundColor = "red";
+btn3.onclick = () => document.body.style.backgroundColor = "#2fd2f7";
+btn4.onclick = () => document.body.style.backgroundColor = "orange";
+btn5.onclick = () => document.body.style.backgroundColor = "pink";
+btn6.onclick = () => document.body.style.backgroundColor = "black";
+btn7.onclick = () => document.body.style.backgroundColor = "yellow";
+btn8.onclick = () => document.body.style.backgroundColor = "green";
+btn9.onclick = () => document.body.style.backgroundColor = "purple";
+btn10.onclick = () => document.body.style.backgroundColor = "#7FFFD4";
